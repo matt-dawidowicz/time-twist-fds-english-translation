37c7c49d5a244f4c151d913887c6c47450d6b423
starlight_world <43389528+matt-dawidowicz@users.noreply.github.com>
2026-09-13 19:46:19 -0400

    Install definitive IPS title authority at package import

--- work/time_twist/__init__.py
+++ work/time_twist/__init__.py
@@ -1,5 +1,8 @@
 """Time Twist FDS inspection and localization helpers."""
 
 from .fds import FdsFile, FdsImage, FdsSide
+from .title_authority import install_definitive_title_authority
+
+install_definitive_title_authority()
 
 __all__ = ["FdsFile", "FdsImage", "FdsSide"]
